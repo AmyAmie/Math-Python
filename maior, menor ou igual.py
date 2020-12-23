f0324dc7c24dab1d4cb8c1efb8ee76c0abac59c9
Leofuchs200811 <leofuchs200811@gmail.com>
2020-12-23 13:58:12 -0300

    Create maior, menor ou igual.py

---
+++ maior, menor ou igual.py
@@ -0,0 +1,8 @@
+num = int(input("Digite um valor: \n"))
+num2 = int(input("Digite o segundo valor: \n"))
+if num == num2:
+    print(f'{num} é igual á {num2}.')
+if num < num2:
+    print(f'{num} é menor do que {num2}.')
+if num > num2:
+    print(f'{num} é maior do que {num2}')
